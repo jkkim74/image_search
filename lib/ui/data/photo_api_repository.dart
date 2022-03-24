b71f83ca7798cb2271590fe3fde942cb99d78133
jack <jack68@naver.com>
2022-03-24 16:53:49 +0900

    Mockito test code make

--- lib/ui/data/photo_api_repository.dart
+++ lib/ui/data/photo_api_repository.dart
@@ -1,4 +1,5 @@
 import '../../Photo.dart';
+import 'package:http/http.dart' as http;
 
 abstract class PhotoApiRepository{
   Future<List<Photo>> fetch(String query);
--- lib/ui/data/pixabay_api.dart
+++ lib/ui/data/pixabay_api.dart
@@ -6,12 +6,13 @@ import '../../Photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi implements PhotoApiRepository{
-  final baseUrl  = "https://pixabay.com/api/";
-  final key = "26185952-d3d85c0548ef1744a92e07988";
+  static const baseUrl  = "https://pixabay.com/api/";
+  static const key = "26185952-d3d85c0548ef1744a92e07988";
 
   @override
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    client ??= http.Client();
+    final response = await client.get(Uri.parse(
         "$baseUrl?key=$key&q=$query&image_type=photo"));
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse["hits"];
